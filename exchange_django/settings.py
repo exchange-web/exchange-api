126d117e9d4c295eb81eac19cc19f4122959f5e6
aslanahmtv <aslanahmtv@gmail.com>
2025-02-05 18:47:23 +0500

    Test

--- exchange_django/settings.py
+++ exchange_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-nfg&+$2_fm8#$c7n(&2^rb(p-2d54f%2)-$$3q_ku2-5!t#&ac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://194.164.245.77"]
 
 
 # Application definition
